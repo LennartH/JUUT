0e0e1a1ab754845d9d3b9060122a38b7491ab128
Lennart Hensler <LennartHensler@aol.com>
2012-12-11 18:58:29 +0100

    Setted the test project up

--- TestJUUT/TestAttributes.cs
+++ TestJUUT/TestAttributes.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Is = NHamcrest.Core.Is;
 
 namespace TestJUUT {
     [TestClass]
     public class TestAttributes {
         [TestMethod]
         public void JUUTTest() {
+            AssertEx.That(1, Is.LessThan(2));
         }
     }
 }
