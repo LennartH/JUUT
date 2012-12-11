4e276c8ff93f4c03589d1e7f3337438b6e308c2e
Lennart Hensler <LennartHensler@aol.com>
2012-12-11 22:28:00 +0100

    Implemented JUUTTest-Attribute

---
+++ JUUT/Attributes/JUUTTestAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUUT.Attributes {
+    /// <summary>
+    /// Attribute to identify test classes of JUUT. Can't be inherated. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class JUUTTestAttribute : Attribute {
+        public JUUTTestAttribute() {}
+    }
+}
--- TestJUUT/TestAttributes.cs
+++ TestJUUT/TestAttributes.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.Reflection;
+
+using JUUT.Attributes;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHamcrest.Core;
 using Is = NHamcrest.Core.Is;
 
 namespace TestJUUT {
     [TestClass]
     public class TestAttributes {
+
         [TestMethod]
         public void JUUTTest() {
-            AssertEx.That(1, Is.LessThan(2));
+            Attribute JUUTTest = typeof (JUUTTestAttributedMock).GetCustomAttribute(typeof (JUUTTestAttribute), true);
+            AssertEx.That(JUUTTest, Is.NotNull());
+
+            JUUTTest = typeof(NotAttributedMock).GetCustomAttribute(typeof(JUUTTestAttribute), true);
+            AssertEx.That(JUUTTest, Is.Null());
         }
+
+        [JUUTTest]
+        private class JUUTTestAttributedMock {
+            
+        }
+
+        private class NotAttributedMock {}
     }
 }
