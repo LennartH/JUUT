26376e5934bedf77aef0ae15804a56c88f59b92f
Lennart Hensler <LennartHensler@aol.com>
2012-12-13 14:49:51 +0100

    Implemented AssertException

---
+++ JUUT/AssertException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JUUT {
+
+    /// <summary>
+    /// Exception to mark a failed assertion.
+    /// </summary>
+    public sealed class AssertException : Exception {
+
+        public AssertException() : base("") { }
+
+        public AssertException(string message) : base(message) { }
+
+    }
+}
---
+++ TestJUUT/TestAssertException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using JUUT;
+
+using Is = NHamcrest.Core.Is;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace TestJUUT {
+    [TestClass]
+    public class TestAssertException {
+        [TestMethod]
+        public void Creation() {
+            AssertException e = new AssertException();
+            AssertEx.That(e.Message, Is.EqualTo(""));
+
+            e = new AssertException("Test");
+            AssertEx.That(e.Message, Is.EqualTo("Test"));
+            try {
+                throw e;
+                Assert.Fail("This line shouldn't be reached. An exception should be thrown.");
+            } catch (AssertException) {
+                // Do nothing
+            }
+        }
+    }
+}
