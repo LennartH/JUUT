fa846d04aac0e671340a4c2c1f34d90d05817bae
Lennart Hensler <LennartHensler@aol.com>
2012-12-13 18:00:49 +0100

    Started to implement the Assert class

---
+++ JUUT/Assert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NHamcrest;
+
+namespace JUUT
+{
+    public class Assert
+    {
+
+        public static void Fail() {
+            Fail("");
+        }
+
+        public static void Fail(string message) {
+            throw new AssertException(message);
+        }
+
+        public static void That<T>(T actual, IMatcher<T> matcher) {
+            That(actual, matcher, "");
+        }
+
+        public static void That<T>(T actual, IMatcher<T> matcher, string message) {
+            if (matcher.Matches(actual)) {
+                return;
+            }
+
+            IDescription description = new StringDescription();
+            matcher.DescribeTo(description);
+
+            IDescription mismatchDescription = new StringDescription();
+            matcher.DescribeMismatch(actual, mismatchDescription);
+
+            throw new AssertException("Expected " + mismatchDescription.ToString() + ", but is " + description.ToString() + ".");
+        }
+
+    }
+}
