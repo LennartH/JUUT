f5ec5cea655ee1312f4199d8ad257d6b963e9591
Lennart Hensler <LennartHensler@aol.com>
2012-12-16 05:13:21 +0100

    Implemented test for class TestReport

---
+++ JUUT/TestReport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUUT {
+
+    public enum TestStatus {
+
+        Failed,
+        Error,
+        Passed
+
+    }
+
+    public class TestReport {
+
+        public TestStatus TestStatus { get; private set; }
+        public string Text { get; private set; }
+
+        private readonly Exception RaisedException;
+        private readonly MemberInfo TestMethod;
+
+        public TestReport(MethodInfo testMethod, Exception raisedException) {
+            if (testMethod == null) {
+                throw new ArgumentException("The test method of a test report can't be null.");
+            }
+
+            this.RaisedException = raisedException;
+            this.TestMethod = testMethod;
+
+            SetStatus();
+            SetText();
+        }
+
+        private void SetText() {
+            if (RaisedException != null) {
+                switch (TestStatus) {
+                    case TestStatus.Failed:
+                        Text = "The " + TestMethod.Name + "-Test failed: " + RaisedException.Message;
+                        break;
+                    case TestStatus.Error:
+                        Text = "The " + TestMethod.Name + "-Test raised an unexpected exception: " + RaisedException.Message;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            } else {
+                Text = "The " + TestMethod.Name + "-Test passed successfully.";
+            }
+        }
+
+        private void SetStatus() {
+            if (RaisedException == null) {
+                TestStatus = TestStatus.Passed;
+            } else if (RaisedException is AssertException) {
+                TestStatus = TestStatus.Failed;
+            } else {
+                TestStatus = TestStatus.Error;
+            }
+        }
+
+    }
+
+}
---
+++ TestJUUT/TestTestReport.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using JUUT;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Throws = NHamcrest.Core.Throws;
+using Is = NHamcrest.Core.Is;
+
+namespace TestJUUT {
+
+    [TestClass]
+    public class TestTestReport {
+
+        [TestMethod]
+        public void Creation() {
+            MethodInfo testMethod = new DynamicMethod("TestName", null, null);
+            Exception raisedException = new AssertException("Exception Text");
+
+            TestReport report = new TestReport(testMethod, raisedException);
+            AssertEx.That(report.TestStatus, Is.EqualTo(TestStatus.Failed));
+
+            raisedException = new NullReferenceException();
+            report = new TestReport(testMethod, raisedException);
+            AssertEx.That(report.TestStatus, Is.EqualTo(TestStatus.Error));
+
+            report = new TestReport(testMethod, null);
+            AssertEx.That(report.TestStatus, Is.EqualTo(TestStatus.Passed));
+
+            AssertEx.That(() => { new TestReport(null, raisedException); }, Throws.An<ArgumentException>());
+        }
+
+        [TestMethod]
+        public void Output() {
+            MethodInfo testMethod = new DynamicMethod("TestName", null, null);
+            Exception raisedException = new AssertException("Exception Text");
+
+            TestReport report = new TestReport(testMethod, raisedException);
+            AssertEx.That(report.Text, Is.EqualTo("The TestName-Test failed: Exception Text"));
+
+            raisedException = new NullReferenceException("Null reference");
+            report = new TestReport(testMethod, raisedException);
+            AssertEx.That(report.Text, Is.EqualTo("The TestName-Test raised an unexpected exception: " + raisedException.Message));
+
+            report = new TestReport(testMethod, null);
+            AssertEx.That(report.Text, Is.EqualTo("The TestName-Test passed successfully."));
+        }
+
+    }
+
+}
