775970d8b4ea8267ff8837da916e6232eb7a0ca0
Lennart Hensler <LennartHensler@aol.com>
2013-05-09 21:50:34 +0200

    Added a unity sample, but the versions are fucked up...

---
+++ UnitySamply/Assets/Scripts/TestStarter.cs
@@ -0,0 +1,13 @@
+using JUUT_Unity;
+
+using UnityEngine;
+using System.Collections;
+
+public class TestStarter : MonoBehaviour {
+
+	// Use this to set up the test suite
+	void Start () {
+	    UnityTestSuite suite = new UnityTestSuite();
+	}
+
+}
---
+++ UnitySamply/Assets/Tests/TestSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using JUUT_Core;
+using JUUT_Core.Attributes;
+using JUUT_Core.Attributes.Methods;
+
+namespace Assets.Tests {
+
+    [JUUTTestClass]
+    public class TestSample {
+
+        [SimpleTestMethod]
+        public void TestMethod() {
+            Assert.That(12, Is.EqualtTo(10));
+        }
+
+    }
+
+}
