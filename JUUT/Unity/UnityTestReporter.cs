4637ff5cd0bb8027edb6672458b56720db2dcd8c
Lennart Hensler <LennartHensler@aol.com>
2013-05-09 20:28:03 +0200

    Implemented the ConsoleReporter.

---
+++ JUUT/Unity/UnityTestReporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JUUT.Core.Reporters;
+
+namespace JUUT.Unity {
+
+    public class UnityTestReporter : ConsoleReporter {
+
+        public override void PresentReports() {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
--- TestJUUT/ReporterTests/TestConsoleReporter.cs
+++ TestJUUT/ReporterTests/TestConsoleReporter.cs
@@ -1,4 +1,16 @@
 ﻿using System;
+
+using JUUT.Core.Attributes;
+using JUUT.Core.Attributes.Methods;
+using JUUT.Core.Reporters;
+using JUUT.Core.Runners;
+
+using NHamcrest.Core;
+
+using TestJUUT.Util;
+
+using Assert = JUUT.Core.Assert;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestJUUT.ReporterTests {
@@ -6,9 +18,63 @@ namespace TestJUUT.ReporterTests {
     [TestClass]
     public class TestConsoleReporter {
 
+        private static string Text;
+
         [TestMethod]
-        public void TestMethod1() {
+        public void TextCreation() {
+            TestReporter reporter = new ConsoleReporterMock();
+
+            TestRunner runner = new SimpleTestRunner(typeof(TestMock));
+            runner.AddAll();
+            runner.Run();
+            reporter.AddReport(runner.Report);
+
+            runner = new SimpleTestRunner(typeof(AnotherTestMock));
+            runner.AddAll();
+            runner.Run();
+            reporter.AddReport(runner.Report);
+
+            reporter.PresentReports();
+            AssertEx.That(Text, Is.EqualTo(
+                    "3 tests runned: 2 failed, 1 succeeded\n\n" +
+                    "The Bar-Method failed: Expected is 2, but was 1.\n\n" +
+                    "The Foo-Method threw an unexpected exception: Error Text."));
         }
+
+        #region Helper Classes
+        private class ConsoleReporterMock : ConsoleReporter {
+
+            public override void PresentReports() {
+                Text = CreateText();
+            }
+
+        }
+
+        [JUUTTestClass]
+        private class TestMock {
+
+            [SimpleTestMethod]
+            public void Foo() {
+
+            }
+
+            [SimpleTestMethod]
+            public void Bar() {
+                Assert.That(1, Is.EqualTo(2));
+            }
+
+        }
+
+        [JUUTTestClass]
+        private class AnotherTestMock {
+
+            [SimpleTestMethod]
+            public void Foo() {
+                throw new NullReferenceException("Error Text.");
+            }
+
+        }
+        #endregion
 
     }
 
