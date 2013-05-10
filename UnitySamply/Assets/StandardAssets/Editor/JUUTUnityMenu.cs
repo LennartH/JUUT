d3efdd125456a9e33989164f5f895356f2e0cf72
Lennart Hensler <LennartHensler@aol.com>
2013-05-10 22:39:07 +0200

    Added a class for the menu in Unity.

---
+++ UnitySamply/Assets/StandardAssets/Editor/JUUTUnityMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using JUUT_Unity;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Assets.StandardAssets.Editor {
+
+    public class JUUTUnityMenu {
+
+        private static void ClearDebugLog() {
+            Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+            MethodInfo method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
+        }
+
+        [MenuItem("JUUT/Run All Tests %#t")]
+        private static void RunAllTests() {
+            ClearDebugLog();
+
+            UnityTestSuite suite = new UnityTestSuite();
+            //FindAndAddAllTestCases(suite);
+            suite.Run();
+        }
+
+    }
+
+}
