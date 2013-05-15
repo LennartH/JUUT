b0a5eb733548540586091ba6dc5025466603068d
Lennart Hensler <LennartHensler@aol.com>
2013-05-15 20:52:03 +0200

    some stuff

--- JUUT/JUUT_Core/Attributes/Methods/TestAfterAttribute.cs
+++ JUUT/JUUT_Core/Attributes/Methods/TestAfterAttribute.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-
-using Spring.Aop.Framework;
 
 namespace JUUT_Core.Attributes.Methods {
 
